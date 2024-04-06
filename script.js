73d73eb28adc7bd5ad5633f7cb319f97cef636d0
Bittu Sharma <144942287+BittuSharma2@users.noreply.github.com>
2024-04-06 15:30:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function pageflip1(){
+    document.getElementById("rotate").style.transform="rotateY(180deg)";
+}
+
+function pageflip2(){
+    document.getElementById("rotate").style.transform="rotateY(0deg)";
+}
+
+
+const data=document.querySelector("#form");
+console.dir(data);
